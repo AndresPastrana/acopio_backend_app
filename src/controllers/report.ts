7e662fb651abefec3d9c66efc27df7f2c45d87ad
andresLnx <andreserluis@gmail.com>
2023-10-06 22:47:14 -0400

    core: add create and read reports

--- src/controllers/report.ts
+++ src/controllers/report.ts
@@ -1,31 +1,44 @@
 import { Request, Response } from "express";
 import { Types } from "mongoose";
 import { handleResponse } from "./../middleware/index.js";
-import { ReportModel } from "./../models/index.js";
+import { ProducerModel, ReportModel, UserModel } from "./../models/index.js";
 
-// TODO: Validations and error handling
-// Validar que es un especiaista y que al trabajador que le esta creando el reporte peretnece a su Base Productiva
-// Validar que el prodcutor existe
-// Validar que ese pructor no tena ningun reporte generado ese dia
-// Asumimos que una vez generado un reporte diario no puede ser editado ni eleminado, solo leido
 const createReport = async (req: Request, res: Response) => {
 	try {
-		const { dayli_collect, producer, productive_base, type_milk } = req.body;
-		const oi_producer = new Types.ObjectId(producer);
-		const oi_productive_base = new Types.ObjectId(productive_base);
-		const report = new ReportModel({
-			producer: oi_producer,
-			productive_base: oi_productive_base,
-			dayli_collect,
-			type_milk,
-		});
-		const result = await report.save();
+		const uid = new Types.ObjectId(req?.user.uid as string);
+		const producer_id = new Types.ObjectId(req.body.producer);
+
+		const authUser = await UserModel.findById(uid);
+		const producer = await ProducerModel.findById(producer_id);
+
+		// TODO chechk that producer.productive_base === authUser.productiveBaseInCharge
+		if (
+			producer?.productive_base.toString() ===
+			authUser?.productiveBaseInCharge.toString()
+		) {
+			const { dayli_collect = 0, type_milk = null } = req.body;
+			const report = new ReportModel({
+				producer: producer_id,
+				productive_base: authUser?.productiveBaseInCharge,
+				dayli_collect,
+				type_milk,
+			});
+			const newReport = await report.save();
+			return handleResponse({
+				data: { report: newReport },
+				msg: "Succes",
+				statusCode: 201,
+				res,
+			});
+		}
+
 		return handleResponse({
-			data: result,
-			msg: "Succes",
-			statusCode: 201,
 			res,
+			statusCode: 401,
+			msg: "The current loged specialist can't generate a report for this porducer",
 		});
+
+		// const result = await report.save();
 	} catch (error) {
 		const err = error as Error;
 		return handleResponse({
@@ -37,53 +50,29 @@ const createReport = async (req: Request, res: Response) => {
 	}
 };
 
-// TODO: validations and error handling
-// Valid token 401
-// que es especialista jefe de la base que genero el reporte--401
-// Required fileds  404
-const getReportById = async (req: Request, res: Response) => {
-	try {
-		const _id = req.params.id;
-		const query = ReportModel.findById(new Types.ObjectId(_id)).populate([
-			"producer",
-			"productive_base",
-		]);
-		// This can be null HANDLE THIS LATER
-		const report = await query.exec();
-		return handleResponse({ res, data: report || {}, statusCode: 200 });
-	} catch (error) {
-		const err = error as Error;
-		return handleResponse({
-			msg: "faliure",
-			statusCode: 404,
-			res,
-			error: err,
-		});
-	}
-};
-
-// TODO: Valdations and error handling
-// Validar campos que tienen que venir en la request 404
-// Validar tipos de datos de esos campos 404
-// Validar token -->401
-// Validar que es especialista  -->401
-// Validar que el trabajador existe--> 404
-// Validar que el especialista es el jefe de la base prod a la que pertenece el trabajador -->401
-
-// ---------->   50...Internal server errror otherwise
 const getAllReportsByProducer = async (req: Request, res: Response) => {
+	const { producer } = req.query;
+
 	try {
-		const producer = req.query.id as string;
-		const oi_producer = new Types.ObjectId(producer);
-		const query = ReportModel.find({ producer: oi_producer });
-		const producer_reports = await query.exec();
-		handleResponse({
+		const uid = new Types.ObjectId(req?.user.uid);
+		const producer_id = new Types.ObjectId(req.query.producer);
+
+		const authUser = await UserModel.findById(uid);
+		const producer = await ProducerModel.findById(producer_id);
+
+		console.log(producer);
+		console.log(authUser);
+
+		const producerReports = await ReportModel.find({
+			producer: producer_id,
+			productive_base: authUser?.productiveBaseInCharge,
+		});
+		return handleResponse({
 			res,
-			data: producer_reports,
+			data: { reports: producerReports },
 			statusCode: 200,
 		});
 	} catch (error) {
-		const err = error as Error;
 		return handleResponse({
 			statusCode: 500,
 			res,
@@ -94,5 +83,4 @@ const getAllReportsByProducer = async (req: Request, res: Response) => {
 export const ReportController = {
 	createReport,
 	getAllReportsByProducer,
-	getReportById,
 };
--- src/routes/report.ts
+++ src/routes/report.ts
@@ -1,26 +1,51 @@
 import { Router } from "express";
-import { isValidToken, protectRouteByRole } from "../middleware/index.js";
-import { Role } from "../types.d.js";
+import { body, query } from "express-validator";
+import {
+	isValidDoc,
+	isValidToken,
+	protectRouteByRole,
+	validateRequest,
+} from "../middleware/index.js";
+import { MilkType, Role } from "../types.d.js";
 import { ReportController } from "./../controllers/index.js";
+import { ProducerModel } from "./../models/index.js";
 export const router = Router();
-
 // POST /report
 router.post(
 	"/",
-	[isValidToken, protectRouteByRole(Role.Specialist)],
+	[
+		isValidToken,
+		protectRouteByRole([Role.Specialist]),
+		body("dayli_collect", "Invalid dayli collect").isInt(),
+		body("type_milk", "Invalid type of milk").isIn([
+			MilkType.cold,
+			MilkType.hot,
+		]),
+		body("dayli_collect", "Invalid dayli collect").isInt(),
+		body("producer", "Invalid producer")
+			.exists({ values: "null" })
+			.custom(async (producer) => {
+				await isValidDoc(producer, ProducerModel, false);
+				return true;
+			}),
+		validateRequest,
+	],
 	ReportController.createReport,
 );
 
-// GET /report/producer?id=3743vn3hcc3n3c53
-router.get(
-	"/producer",
-	[isValidToken, protectRouteByRole(Role.Specialist)],
-	ReportController.getAllReportsByProducer,
-);
+// GET /report?producer=3743vn3hcc3n3c53
 
-// GET /report/reportid
 router.get(
-	"/:id",
-	[isValidToken, protectRouteByRole(Role.Specialist)],
-	ReportController.getReportById,
+	"/",
+	[
+		isValidToken,
+		protectRouteByRole([Role.Specialist]),
+		query("producer", "Invalid producer")
+			.exists({ values: "null" })
+			.custom(async (producer) => {
+				await isValidDoc(producer, ProducerModel, false);
+				return true;
+			}),
+	],
+	ReportController.getAllReportsByProducer,
 );
